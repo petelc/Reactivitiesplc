5365a845f589bacf0c337e194c0a617e21e625b2
Peter <petelc1966@hotmail.com>
2022-07-11 03:32:46 -0400

    End of Section 14

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Text;
+using System.Threading.Tasks;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,6 +39,17 @@ namespace API.Extensions
                         ValidateAudience = false,
                     };
                 });
+
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("IsActivityHost", policy =>
+                {
+                    policy.Requirements.Add(new IsHostRequirement());
+                });
+            });
+
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+            
             services.AddScoped<TokenService>();
 
             return services;
