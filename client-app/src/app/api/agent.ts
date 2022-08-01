f690a0295d3a766b7c41cbb593e193d33464c57d
Peter <petelc1966@hotmail.com>
2022-08-01 03:04:55 -0400

    Heroku Deploy

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -35,12 +35,9 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError) => {
-    const { data, status, config } = error.response!;
+    const { data, status, config, headers } = error.response!;
     switch (status) {
       case 400:
-        if (typeof data === 'string') {
-          toast.error(data);
-        }
         if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
           history.push('/not-found');
         }
@@ -52,10 +49,20 @@ axios.interceptors.response.use(
             }
           }
           throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
         }
         break;
       case 401:
-        toast.error('unauthorized');
+        if (
+          status === 401 &&
+          headers['www-authenticate']?.startsWith(
+            'Bearer error="invalid_token"'
+          )
+        ) {
+          store.userStore.logout();
+          toast.error('Session expired - please login again');
+        }
         break;
       case 404:
         history.push('/not-found');
